a45ff9d6e8e834a8094a1d03f88ddf6db1c5b74f
homen3 <info@gianlucamazza.it>
2017-04-06 12:58:35 +0200

    updated ports

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -45,7 +45,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 19332;
+        nRPCPort = 11332;
         strDataDir = "testnet4";
     }
 };
@@ -59,7 +59,7 @@ class CBaseRegTestParams : public CBaseChainParams
 public:
     CBaseRegTestParams()
     {
-        nRPCPort = 19332;
+        nRPCPort = 11332;
         strDataDir = "regtest";
     }
 };
